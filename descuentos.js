828dc9e3fea57a174e9afe757c4b65320a3724d0
jgmzur29 <joagomezur29@gmail.com>
2022-06-02 10:25:33 -0500

    Se modifico el codigo(html y js)  para que los descuentos funcionen por medio de cupones

--- descuentos.js
+++ descuentos.js
@@ -1,19 +1,43 @@
 
-function calcularPrecioConDescuento(precio, descuento) {
-    const porcentajePrecioConDescuento = 100 - descuento;
-    const precioConDescuento = (precio*porcentajePrecioConDescuento)/100;
-
-    return (precioConDescuento);
+function calcularPrecioConDescuento(precio,descuento){
+    const porcentajePrecioConDescuento=100-descuento;
+    const precioConDescuento=(precio*porcentajePrecioConDescuento)/100;
+    return precioConDescuento;
 }
 
-function onClickPriceDiscount () {
-    const inputPrice = (document.getElementById("inputPrice"));
-    const price = inputPrice.value;
-    const inputDiscount = (document.getElementById("inputDiscount"));
-    const discount = inputDiscount.value;
+const coupons = [
+    "15Loco",
+    "25Secreto",
+    "30Fantastico",
+    "elDeLosDevOps"
+];
+console.log(coupons);
 
-    const precioFinal = calcularPrecioConDescuento(price, discount);
+function onClickPriceDiscount() {
+    const inputPrice = document.getElementById("inputPrice");
+    const priceValue = inputPrice.value;
+
+    const inputCoupon = document.getElementById("inputCoupon");
+    const couponValue = inputCoupon.value;
+    let descuento;
+
+    switch(couponValue) {
+      case coupons[0]:
+        descuento = 15;
+      break;
+      case coupons[1]:
+        descuento = 25;
+      break;
+      case coupons[2]:
+        descuento = 30;
+      break;
+      case coupons[3]:
+          descuento = 50;
+      break;
+    }
+
+    const precioConDescuento = calcularPrecioConDescuento(priceValue, descuento);
     const resultP = document.getElementById("resultPrice");
-    resultP.innerText = "el precio con descuento es $: " + precioFinal;
-}
+    resultP.innerText = "El precio con descuento son: $" + precioConDescuento;
+  }
 
